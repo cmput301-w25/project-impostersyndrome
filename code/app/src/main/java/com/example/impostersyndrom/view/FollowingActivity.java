4f2f6ddfc76e1e4c425ddc9e736c53f032d31b86
Roshan1299 <banisettirosh@gmail.com>
2025-03-17 14:55:50 -0600

    Implemented Profile Nav in search and following screens

--- code/app/src/main/java/com/example/impostersyndrom/view/FollowingActivity.java
+++ code/app/src/main/java/com/example/impostersyndrom/view/FollowingActivity.java
@@ -72,12 +72,9 @@ public class FollowingActivity extends AppCompatActivity {
         });
 
         profileButton.setOnClickListener(v -> {
-            auth.signOut();
-            Toast.makeText(FollowingActivity.this, "Logged out successfully!", Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(FollowingActivity.this, LoginActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-            finish();
+            startActivity(new Intent(FollowingActivity.this, ProfileActivity.class));
         });
+
+
     }
 }
