ad86f360f0eff27abeb1c3baaf6e4fc868ff8575
Roshan1299 <banisettirosh@gmail.com>
2025-03-19 01:18:39 -0600

    Implemented google-sign in

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -58,5 +58,7 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation("androidx.test.espresso:espresso-idling-resource:3.5.1")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
 
 }
