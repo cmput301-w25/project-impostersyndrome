b533662877f707f4167e2011f827278c97188d71
Roshan1299 <banisettirosh@gmail.com>
2025-03-20 03:36:57 -0600

    Made SpotifyAuthService

--- code/app/src/main/java/com/example/impostersyndrom/network/SpotifyAuthService.java
+++ code/app/src/main/java/com/example/impostersyndrom/network/SpotifyAuthService.java
@@ -3,14 +3,14 @@ package com.example.impostersyndrom.network;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 
 public interface SpotifyAuthService {
     @FormUrlEncoded
     @POST("api/token")
     Call<SpotifyAuthResponse> getAccessToken(
-            @Field("grant_type") String grantType,
-            @Field("client_id") String clientId,
-            @Field("client_secret") String clientSecret
+            @Header("Authorization") String authorization,
+            @Field("grant_type") String grantType
     );
 }
