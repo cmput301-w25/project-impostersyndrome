67a123e95e3c64aae370d59a4f8fee98413f3c2a
Roshan1299 <banisettirosh@gmail.com>
2025-03-20 03:36:28 -0600

    Made SpotifyAuthResponse

--- code/app/src/main/java/com/example/impostersyndrom/network/SpotifyAuthResponse.java
+++ code/app/src/main/java/com/example/impostersyndrom/network/SpotifyAuthResponse.java
@@ -4,4 +4,6 @@ public class SpotifyAuthResponse {
     public String access_token;
     public String token_type;
     public int expires_in;
+    public String error;
+    public String error_description;
 }
