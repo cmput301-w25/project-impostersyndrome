31037f17201061621155fbf8c8108dc3efc3275b
Roshan1299 <banisettirosh@gmail.com>
2025-03-21 00:36:16 -0600

    Added album field to SpotifyRecommendationResponse.Track

--- code/app/src/main/java/com/example/impostersyndrom/network/SpotifyRecommendationResponse.java
+++ code/app/src/main/java/com/example/impostersyndrom/network/SpotifyRecommendationResponse.java
@@ -20,10 +20,29 @@ public class SpotifyRecommendationResponse {
 
         @SerializedName("uri")
         public String uri;
+
+        @SerializedName("album") // Add the album field
+        public Album album;
     }
 
     public static class Artist {
         @SerializedName("name")
         public String name;
+    }
+
+    public static class Album {
+        @SerializedName("images")
+        public List<Image> images;
+    }
+
+    public static class Image {
+        @SerializedName("url")
+        public String url;
+
+        @SerializedName("height")
+        public int height;
+
+        @SerializedName("width")
+        public int width;
     }
 }
