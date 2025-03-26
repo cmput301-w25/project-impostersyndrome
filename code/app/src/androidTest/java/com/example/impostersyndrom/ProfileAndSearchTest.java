ed10ed9d3f97d3c21f792fd7eef7a4686efc7844
Roshan1299 <banisettirosh@gmail.com>
2025-03-26 00:30:00 -0600

    Implemented testViewOwnProfile

--- code/app/src/androidTest/java/com/example/impostersyndrom/ProfileAndSearchTest.java
+++ code/app/src/androidTest/java/com/example/impostersyndrom/ProfileAndSearchTest.java
@@ -78,6 +78,24 @@ public class ProfileAndSearchTest {
                 .check(matches(withText(TEST_USERNAME)));
     }
 
+    @Test
+    public void testSearchForAnotherUser() throws Exception {
+        // 1. Go to search screen
+        onView(withId(R.id.searchButton)).perform(click());
+
+        // 2. Wait for search screen to load
+        waitForView(withId(R.id.searchInput), 5000);
+
+        // 3. Enter a known username (search triggers automatically via TextWatcher)
+        onView(withId(R.id.searchInput)).perform(typeText(SEARCH_USERNAME));
+        closeSoftKeyboard();
+        onView(isRoot()).perform(waitFor(2000)); // Wait 2s for ListView to update
+
+        // 4. Confirm username is displayed in the ListView
+        Log.d("TestDebug", "Checking for username: " + SEARCH_USERNAME);
+        onView(allOf(withId(R.id.usernameTextView), withText(SEARCH_USERNAME)))
+                .check(matches(isDisplayed()));
+    }
 
     // Custom wait action
     public static ViewAction waitFor(long delay) {
